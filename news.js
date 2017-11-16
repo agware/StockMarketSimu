a37a86196f1709a29ab4d4c7bbc18ef5de38076e
agware <alex.g.ware@gmail.com>
2017-11-16 13:49:50 +1100

    Updates from feedback added

--- news.js
+++ news.js
@@ -7,33 +7,39 @@ function initNews() {
         .attr('height', '100%');
 
     newsSVG.append('text')
-        .attr('x', '10%')
+        .attr('x', '5%')
         .attr('y', '20%')
         .attr('class', 'news')
         .attr('id', 'news-time')
         .text('');
 
     newsSVG.append('text')
-        .attr('x', '10%')
+        .attr('x', '5%')
         .attr('y', '50%')
         .attr('class', 'news')
         .attr('id', 'news-phrase')
         .text('');
 
     newsSVG.append('text')
-        .attr('x', '10%')
+        .attr('x', '5%')
         .attr('y', '85%')
         .attr('class', 'news')
         .attr('id', 'news-reaction')
         .text('');
 }
 
-function updateNews() {
+function updateNews(stock) {
     let news = [
-        {phrase: "World Alcohol Shortage", reaction: "Market Goes Down!", value: -1, probability: 0.2},
+        {phrase: "World Alcohol Shortage", reaction: "Market Goes Down!", value: -1, probability: 0.1},
+        {phrase: "The Whole World is Hungover", reaction: "Market Goes Down!", value: -1, probability: 0.1},
+        {phrase: "North Korea Stockpiles Drinks", reaction: "Market Goes Down!", value: -1, probability: 0.1},
+        {phrase: "Beers in Great Demand", reaction: "Market Goes Up!", value: 1, probability: 0.1},
         {phrase: "Alcohol is Healthy!", reaction: "Market Goes Up!", value: 1, probability: 0.1},
-        {phrase: "No Alcohol News", reaction: "", value: 0, probability: 0.6},
-        {phrase: "Beers in Great Demand", reaction: "Market Goes Up!", value: 1, probability: 0.1}
+        {phrase: "The Yes Vote Wins!", reaction: "Market Goes Up!", value: 1, probability: 0.1},
+        {phrase: "Exams are Finished!", reaction: "Market Goes Up!", value: 1, probability: 0.1},
+        {phrase: "Vodka Cures Diabetes!", reaction: "Market Goes Up!", value: 1, probability: 0.1},
+        {phrase: "The Cricket Season Starts!", reaction: "Market Goes Up!", value: 1, probability: 0.1},
+        {phrase: "Drinking Games Allowed Again!", reaction: "Market Goes Up!", value: 1, probability: 0.1}
     ];
 
     // Update news
@@ -52,5 +58,12 @@ function updateNews() {
     d3.select('#news-phrase').text(news[newsIndex].phrase);
     d3.select('#news-reaction').text(news[newsIndex].reaction);
 
-    return news[newsIndex].value;
+    if(news[newsIndex].value < 0) {
+        stock -= 1;
+    } else if(news[newsIndex].value > 0) {
+        stock += 1;
+    }
+
+
+    return stock;
 }
